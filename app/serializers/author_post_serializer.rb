4eec15acc903bf86a276571182a8e8564cadb30f
fhides karambu <fhidesjoseph@ gmail.com>
2022-10-03 16:03:59 +0300

    commit

--- app/serializers/author_post_serializer.rb
+++ app/serializers/author_post_serializer.rb
@@ -1,5 +1,5 @@
 class AuthorPostSerializer < ActiveModel::Serializer
-  attributes :title, :short_content
+  attributes :title, :short_content, :tags
   def short_content
     "#{self.object.content[0..39]...}"
   end
