9e4babcbf6d8f0ec1a1b75dbb6737048735cbaa7
fhides karambu <fhidesjoseph@ gmail.com>
2022-10-03 16:00:03 +0300

    commit

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,13 +3,13 @@ class AuthorsController < ApplicationController
 
   def index
     authors = Author.all 
-    render json: authors
+    render json: authors, include: ['posts', 'posts.tags', 'profile']
   end
 
   def show
     author = Author.find(params[:id])
-    render json: author
-  end
+    render json: author, include: ['posts', 'posts.tags', 'profile']
+  end 
 
   private
 
